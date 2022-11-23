ce0097a42065517a120a734ff77ed28cfc574cdd
geng <geng@MacBookPro.local>
2022-11-23 22:24:17 +0800

    all fininshed for role

--- src/roles/index.js
+++ src/roles/index.js
@@ -490,6 +490,103 @@ call p('${handleData.capital(req.body.label)}','${req.body.menu}','${req.body.ro
 
   connection.end();
 });
+roles.post("/editrootmenuall", (req, res) => {
+  //let moackData = req.body;
+  //console.log(req.body)
+  let superSql = `
+  drop procedure if exists p100;
+CREATE PROCEDURE p100(parent varchar(255),currentData varchar(255),children_router JSON,currentIcon varchar(255))
+BEGIN
+DECLARE counter INT DEFAULT (SELECT COUNT(*) from roles);
+DECLARE incre INT DEFAULT 0;
+DECLARE myid INT DEFAULT 0;
+DECLARE myrouter VARCHAR(40);
+DECLARE mymenu VARCHAR(40);
+DECLARE child_count_router INT DEFAULT 0;
+DECLARE mychild_router VARCHAR(40);
+
+DECLARE child_count_menu INT DEFAULT 0;
+DECLARE mychild_menu VARCHAR(40);
+label: LOOP
+	IF incre >= counter THEN
+		LEAVE label; 
+	END IF; 
+	
+	SET child_count_router = 0;
+	SET child_count_menu = 0;
+	SET myid = (SELECT id FROM (SELECT id FROM roles ORDER BY id ASC LIMIT incre,1 ) aa);
+	
+	##router repeat
+	#ok
+	REPEAT
+		SET @originJson = (SELECT router_list from roles where id = myid);
+		SET mychild_router = JSON_EXTRACT(children_router, CONCAT("$[",child_count_router,"]"));
+		SET myrouter = (SELECT (JSON_SEARCH((select router_list from roles ORDER BY id ASC LIMIT incre,1),'one',JSON_UNQUOTE(mychild_router))));
+		SET child_count_router = child_count_router + 1;
+		SET @originDataRouter = JSON_EXTRACT(@originJson,JSON_UNQUOTE(myrouter));
+		SET @newDataRouter = REPLACE(@originDataRouter,LOWER(parent),LOWER(currentData));
+		
+		IF (!(myrouter <=> null)) THEN
+			update roles set router_list =json_replace(router_list, JSON_UNQUOTE(myrouter),JSON_UNQUOTE(@newDataRouter)) where id = myid;
+		END IF; 
+	UNTIL child_count_router = JSON_LENGTH(children_router) END REPEAT;
+	#ok
+	
+	#menu repeat
+	#ok
+	REPEAT
+		SET @originJson = (SELECT menu_list from roles where id = myid);
+		SET mychild_menu = JSON_EXTRACT(children_router, CONCAT("$[",child_count_menu,"]"));
+		SET mymenu = (SELECT (JSON_SEARCH((select menu_list from roles ORDER BY id ASC LIMIT incre,1),'one',JSON_UNQUOTE(mychild_menu))));
+		SET child_count_menu = child_count_menu + 1;
+		
+		SET @originDataMenu = JSON_EXTRACT(@originJson,JSON_UNQUOTE(mymenu));
+		SET @newDataMenu = REPLACE(@originDataMenu,LOWER(parent),LOWER(currentData));
+		SELECT @newDataMenu;
+		IF (!(mymenu <=> null)) THEN
+			update roles set menu_list =json_replace(menu_list, JSON_UNQUOTE(mymenu),JSON_UNQUOTE(@newDataMenu)) where id = myid;
+		END IF; 
+	UNTIL child_count_menu = JSON_LENGTH(children_router) END REPEAT;
+	
+	
+	SET @uniqueMenuLabel = (SELECT (JSON_SEARCH((select menu_list from roles ORDER BY id ASC LIMIT incre,1),'one',JSON_UNQUOTE(parent))));
+	SET @uniqueMenuKey = (SELECT (JSON_SEARCH((select menu_list from roles ORDER BY id ASC LIMIT incre,1),'one',JSON_UNQUOTE(CONCAT('/',LOWER(parent))))));
+	SET @uniqueMenuIcon = regexp_replace(JSON_SEARCH((select menu_list from roles ORDER BY id ASC LIMIT incre,1),'one',JSON_UNQUOTE(parent)),'\.([^\.]*)$','.icon"');
+	
+	update roles set menu_list =json_replace(menu_list, JSON_UNQUOTE(@uniqueMenuKey),JSON_UNQUOTE(CONCAT('/',LOWER(currentData)))) where id = myid;
+	update roles set menu_list =json_replace(menu_list, JSON_UNQUOTE(@uniqueMenuLabel),JSON_UNQUOTE(currentData)) where id = myid;
+	
+	
+	update roles set menu_list =json_replace(menu_list, JSON_UNQUOTE(@uniqueMenuIcon),JSON_UNQUOTE(currentIcon)) where id = myid;
+	#select @uniqueMenuIcon;
+	SET incre = incre + 1;
+END LOOP label;
+	SELECT "OK";
+END;
+
+
+call p100('${handleData.capital(req.body.orginLabel)}','${handleData.capital(req.body.newLabel)}','${JSON.stringify(req.body.originChildren)}','${req.body.newIcon}');
+  `;
+  const connection = config.createConnection({ multipleStatements: true });
+  connection.query(superSql, (err, rows, fields) => {
+    console.log(err)
+    if (err === null) {
+      res.send({
+        status: "OK",
+        message: "edit root sub all successfully!",
+      });
+    } else {
+      console.log(err);
+      res.send(
+        handleData.responseJSON(false, {
+          error: "system error in edit root sub all menu",
+        })
+      );
+    }
+  });
+
+  connection.end();
+});
 // test express mysql
 roles.post("/test", (req, res) => {
   const connection = config.createConnection({ multipleStatements: true });
